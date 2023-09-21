2a1bef9dabe00bd0f5685a551b4512cadcdafb6f
leijason22 <leijason22@gmail.com>
2023-09-20 23:04:35 -0400

    initial constructor, equals method, and parameterized constructor

--- src/Event.java
+++ src/Event.java
@@ -5,8 +5,45 @@ public class Event implements Comparable<Event>{
     private Contact contact;
     private int duration;
 
+    /**
+    * default constructor; no-args constructor
+    * */
+    public Event(){
+        //set initial values for the instance variables.
+        date = null;
+        startTime = null;
+        location = null;
+        contact = null;
+        duration = 0;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj instanceof Event){
+            Event event = (Event) obj;
+            return event.date.equals(this.date) && event.startTime.equals(this.startTime)
+                    && event.location.equals(this.location);
+        }
+        return false;
+    }
     @Override
     public int compareTo(Event o) {
         return 0;
     }
+
+    /**
+     * Parameterized constructor
+     * @param date
+     * @param startTime
+     * @param location
+     */
+    public Event(Date date, Timeslot startTime, Location location){
+        this.date = date;
+        this.startTime = startTime;
+        this.location = location;
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
