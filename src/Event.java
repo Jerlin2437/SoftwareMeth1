b1026805fb0c2a452fa71b915d127546721611a9
leijason22 <leijason22@gmail.com>
2023-09-22 17:22:53 -0400

    Event updates

--- src/Event.java
+++ src/Event.java
@@ -63,14 +63,37 @@ public class Event implements Comparable<Event>{
     public boolean equals(Object obj){
         if (obj instanceof Event){
             Event event = (Event) obj;
-            return event.date.equals(this.date) && event.startTime.equals(this.startTime)
-                    && event.location.equals(this.location);
+            return event.getDate().equals(this.date) && event.getStartTime().equals(this.startTime)
+                    && event.getLocation().equals(this.location);
         }
         return false;
     }
+
+    //compare event date and timeslot
     @Override
-    public int compareTo(Event o) {
+    public int compareTo(Event event) {
+        if ((this.date.compareTo(event.date) > 0)){
+            return 1;
+        }
+        if ((this.date.compareTo(event.date) < 0)){
+            return -1;
+        }
+        if ((this.startTime.compareTo(event.startTime) > 0)){
+            return 1;
+        }
+        if ((this.startTime.compareTo(event.startTime) < 0)){
+            return -1;
+        }
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "[Event Date: " + date + "] " +
+                "[Start: " + startTime + "] " +
+                "[End: " + startTime + duration + "] " +
+                "@" + location +
+                "[Contact: " + contact + "] ";
     }
 
     /**
@@ -78,14 +101,14 @@ public class Event implements Comparable<Event>{
      * @param date
      * @param startTime
      * @param location
+     * @param contact
+     * @param duration
      */
-    public Event(Date date, Timeslot startTime, Location location){
+    public Event(Date date, Timeslot startTime, Location location, Contact contact, int duration) {
         this.date = date;
         this.startTime = startTime;
         this.location = location;
+        this.contact = contact;
+        this.duration = duration;
     }
-    public static void main(String[] args) {
-
-    }
-
 }
