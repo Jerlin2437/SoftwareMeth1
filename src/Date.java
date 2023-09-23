feb4efc1373e38054cf056424592695fa185025f
Jerlin2437 <yuenbo3@gmail.com>
2023-09-23 16:40:00 -0400

    date class complete

--- src/Date.java
+++ src/Date.java
@@ -9,9 +9,50 @@ public class Date implements Comparable<Date>{
     private int year;
     private int month;
     private int day;
-    public boolean isValid(){
 
-        return false;
+    public Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public boolean isValid() {
+        if (year < 0 || month < 1 || month > 12)
+            return false;
+
+        switch (month) {  // January
+            // March
+            // May
+            // July
+            // August
+            // October
+            // December
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                return day >= 1 && day <= DAYS_31;
+            }
+            /** April
+            June
+            September
+            November */
+            case 4, 6, 9, 11 -> {
+                return day >= 1 && day <= DAYS_30;
+            }
+            // February
+            case 2 -> {  // February
+                if (isLeapYear()) {
+                    return day >= 1 && day <= DAYS_FEB_LEAP;
+                } else {
+                    return day >= 1 && day <= DAYS_FEB_NORMAL;
+                }
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+
+    private boolean isLeapYear() {
+        return (year % QUADRENNIAL == 0 && year % CENTENNIAL != 0) || (year % QUATERCENTENNIAL == 0);
     }
     @Override
     public int compareTo(Date otherDate) {
