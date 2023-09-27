65c135b5a9dcdea798b77597ca341bcc650645e4
Jerlin2437 <yuenbo3@gmail.com>
2023-09-27 16:27:57 -0400

    Update EventOrganizer.java

--- src/EventOrganizer.java
+++ src/EventOrganizer.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class EventOrganizer {
-    /** All commands
+    /**
+     * All commands
      * R - cancel an event and remove from calendar
      * P - display event calendar with current order
      * PE - display event calendar sorted by event date and timeslot
@@ -9,7 +12,27 @@ public class EventOrganizer {
      */
     public void run() {
         System.out.println("Event Organizer running....");
-
+        scan();
         System.out.println("Event Organizer Terminated");
+
     }
+    public void scan(){
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.equalsIgnoreCase("Q")) {
+                break;
+            }
+            Scanner lineScanner = new Scanner(line);
+            // Tokenize each line by reading each token
+            while (lineScanner.hasNext()) {
+                String token = lineScanner.next();
+            }
+
+        }
+    }
+    public void errorMessages(){
+
+    }
+    
 }
