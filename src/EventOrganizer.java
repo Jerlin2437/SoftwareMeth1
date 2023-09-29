760e15c3f61c42a64740421769ca096c34f55e91
Jerlin2437 <yuenbo3@gmail.com>
2023-09-29 15:23:55 -0400

    Update EventOrganizer.java

--- src/EventOrganizer.java
+++ src/EventOrganizer.java
@@ -18,7 +18,8 @@ public class EventOrganizer {
         System.out.println("Event Organizer Terminated");
 
     }
-    public void scan(EventCalendar eventCalendar){
+
+    public void scan(EventCalendar eventCalendar) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
@@ -29,53 +30,120 @@ public class EventOrganizer {
             // Tokenize each line by reading each token
             while (lineScanner.hasNext()) {
                 String token = lineScanner.next();
-                if (token.equals("A")){
-
-                }
-                else if(token.equals("R")){
+                if (token.equals("A")) {
+                    addEvent(token, eventCalendar);
+                } else if (token.equals("R")) {
                     cancelEvent(token, eventCalendar);
-                }
-                else if(token.equals("P")){
+                } else if (token.equals("P")) {
 
-                }
-                else if(token.equals("PE")){
+                } else if (token.equals("PE")) {
 
-                }
-                else if(token.equals("PC")){
+                } else if (token.equals("PC")) {
 
-                }
-                else if(token.equals("PD")){
+                } else if (token.equals("PD")) {
                 }
             }
         }
     }
-    public static void addEvent(String line, EventCalendar eventCalendar){
 
-    }
-    public static void cancelEvent(String line, EventCalendar eventCalendar){
+    public static void addEvent(String line, EventCalendar eventCalendar) {
         Scanner lineScanner = new Scanner(line);
-        lineScanner.next();
-        String token = lineScanner.next();
+        lineScanner.next(); // Assuming this is used to skip some initial token (not clear from the context)
 
-        Date date = extractDate(token);
-        if (date.isValid() && date.isFuture() && date.isUnder6Months()) {
-            token = lineScanner.next();
-            token = token.toUpperCase();
-            Timeslot timeslot = extractTimeslot(token);
-            if (timeslot != null){
-                token = lineScanner.next();
-                token = token.toUpperCase();
-                Location location = extractLocation(token);
-                if (location != null){
-                    Event event = new Event(date, timeslot, location);
-                    eventCalendar.remove(event);
-                }
-            }
+        Date date = extractDate(lineScanner.next());
 
+        if (!date.isValid() || !date.isFuture() || !date.isUnder6Months()) {
+            return;  // Guard clause
+        }
 
+        String timeslotToken = lineScanner.next().toUpperCase();
+        Timeslot timeslot = extractTimeslot(timeslotToken);
+
+        if (timeslot == null) {
+            return;  // Guard clause
+        }
+
+        String locationToken = lineScanner.next().toUpperCase();
+        Location location = extractLocation(locationToken);
+
+        if (location == null) {
+            return;
+        }
+        String departmentToken = lineScanner.next().toUpperCase();
+        String emailToken = lineScanner.next();
+        Contact contact = extractContact(departmentToken, emailToken);
+
+        if (contact == null) {
+            return;
+        }
+        int duration = Integer.parseInt(lineScanner.next());
+        if (duration >= 30 && duration <= 120) {
+            Event event = new Event(date, timeslot, location, contact, duration);
+            if (!eventCalendar.contains(event)) {
+                System.out.println("Event added to the calendar.");
+                eventCalendar.add(event);
+            } else
+                System.out.println("The event is already on the calendar.");
         }
     }
-    public static Location extractLocation(String locationCode){
+
+    public static void cancelEvent(String line, EventCalendar eventCalendar) {
+        Scanner lineScanner = new Scanner(line);
+        lineScanner.next(); // Assuming this is used to skip some initial token (not clear from the context)
+
+        Date date = extractDate(lineScanner.next());
+
+        if (!date.isValid() || !date.isFuture() || !date.isUnder6Months()) {
+            return;  // Guard clause
+        }
+
+        String timeslotToken = lineScanner.next().toUpperCase();
+        Timeslot timeslot = extractTimeslot(timeslotToken);
+
+        if (timeslot == null) {
+            return;  // Guard clause
+        }
+
+        String locationToken = lineScanner.next().toUpperCase();
+        Location location = extractLocation(locationToken);
+
+        if (location != null) {
+            Event event = new Event(date, timeslot, location);
+            if (eventCalendar.remove(event))
+                System.out.println("Event has been removed from the calendar!");
+            else
+                System.out.println("Cannot remove; event is not in the calendar!");
+        }
+    }
+
+
+    public static Contact extractContact(String departmentCode, String emailCode) {
+        if (emailCode.endsWith("@rutgers.edu")) {
+            switch (departmentCode) {
+                case "CS" -> {
+                    return new Contact(Department.CS, emailCode);
+                }
+                case "EE" -> {
+                    return new Contact(Department.EE, emailCode);
+                }
+                case "BAIT" -> {
+                    return new Contact(Department.BAIT, emailCode);
+                }
+                case "ITI" -> {
+                    return new Contact(Department.ITI, emailCode);
+                }
+                case "MATH" -> {
+                    return new Contact(Department.MATH, emailCode);
+                }
+                default -> {
+                    System.out.println("Invalid contact information!");
+                    return null;
+                }
+            }
+        }
+    }
+
+    public static Location extractLocation(String locationCode) {
         switch (locationCode) {
             case "HLL114" -> {
                 return Location.HLL114;
@@ -101,7 +169,8 @@ public class EventOrganizer {
             }
         }
     }
-    public static Timeslot extractTimeslot(String timeslot){
+
+    public static Timeslot extractTimeslot(String timeslot) {
         switch (timeslot) {
             case "MORNING" -> {
                 return Timeslot.MORNING;
@@ -116,24 +185,27 @@ public class EventOrganizer {
         }
         return null;
     }
-    public static Date extractDate(String stringDate){
+
+    public static Date extractDate(String stringDate) {
         StringTokenizer tokenizer = new StringTokenizer(stringDate, "/");
         int month = Integer.parseInt(tokenizer.nextToken());
         int day = Integer.parseInt(tokenizer.nextToken());
         int year = Integer.parseInt(tokenizer.nextToken());
         Date date = new Date(year, month, day);
-        if(!date.isValid())
+        if (!date.isValid())
             System.out.println(date.toString() + ": Invalid calendar date!");
-        else if(!date.isFuture())
+        else if (!date.isFuture())
             System.out.println(date.toString() + ": Event date must be a future date!");
-        else if(!date.isUnder6Months())
+        else if (!date.isUnder6Months())
             System.out.println(date.toString() + ": Event date must be within 6 months!");
         return date;
     }
-    public static void errorMessages(){
+
+    public static void errorMessages() {
 
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
     }
 
 }
